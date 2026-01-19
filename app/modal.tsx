781af3cdccb09c03622eb35752154f1e7ed3dd9e
A-4-Atom <vcikas2626@gmail.com>
2026-01-19 23:17:08 +0530

    initial project setup with nativewind

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,17 +1,9 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { StyleSheet, Text } from 'react-native';
 
 export default function ModalScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+  return (<>
+    <Text style={styles.container}>This is a modal screen.</Text>
+  </>
   );
 }
 
